270480f9347bcca3b4a52b5164b6bd909d028adf
Josh Hoyt <josh.hoyt@galois.com>
2010-12-24 13:15:47 -0800

    Added updated j3h example

--- examples/j3h/alpacas.hs
+++ examples/j3h/alpacas.hs
@@ -1,34 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Control.Applicative ( (<|>) )
 import Alpacas
-import Alpacas.Page ( respondPage, renderPage, modifyBody, addCss, Stylesheet(..) )
-import Alpacas.EditConfig ( editConfig )
-import Alpacas.ReadWriteFile ( editFile )
-import Alpacas.Types ( Config(..) )
-import qualified Config.Dyre as Dyre
-import Snap.Types
-import Snap.Util.FileServe ( fileServe )
+import Alpacas.Types
+import Alpacas.Page ( appendBody )
 import System.FilePath ( (</>) )
-import Text.Blaze.Html5 ( (!) )
-import Text.Blaze.Renderer.Utf8 ( renderHtml )
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import qualified Data.Text as T
-
-app :: Dyre.Params Config -> Config -> Snap ()
-app params cfg =
-    ifTop (respondPage (renderer cfg) (statusPage cfg))<|>
-    route [ ("reload", reloadServer)
-          , ("edit-config", editConfig params (renderer cfg))
-          , ("edit-css", editFile (pfx </> "css" </> "default.css") (renderer cfg))
-          , ("css", fileServe $ pfx </> "css")
-          , ( "js"
-            , fileServe (pfx </> "js") <|>
-              fileServe (pfx </> "codemirror" </> "js")
-            )
-          ]
 
 pfx :: FilePath
 pfx = "/home/j3h/mine/alpacas/"
@@ -44,15 +20,5 @@ ghcOpts = [ "-i" `ifx` "src"
 
 main :: IO ()
 main = alpacasMain ghcOpts $ \p ->
-       (defaultConfig p)
-       { cfgApp = app p
-       , renderer = render
-       }
-    where
-      render = renderPage . addCss css . modifyBody addNav
-      addNav h = navControls navItems >> h
-      navItems = NavItem "/" "home" :
-                 NavItem "/edit-css" "edit css" :
-                 NavItem "/edit-config" "edit config" :
-                 NavItem "/reload" "reload server" : []
-      css = StylesheetLink "/css/default.css"
+           let cfg = defaultConfig p
+           in cfg { renderer = renderer cfg }
